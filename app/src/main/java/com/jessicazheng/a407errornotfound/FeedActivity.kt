e2e3dc923c5f5c70fc6ec05b3b63f1f2d9ebfe20
Yesenia <ramirezy@lafayette.edu>
2021-08-12 21:07:44 -0500

    Adding feed

---
+++ app/src/main/java/com/jessicazheng/a407errornotfound/FeedActivity.kt
@@ -0,0 +1,11 @@
+package com.jessicazheng.a407errornotfound
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FeedActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.items)
+    }
+}
